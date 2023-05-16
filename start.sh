b430cae7690f213b6127c0568cccef7b91cacdce
Danil Nasibullin <grandvitaro07@gmail.com>
2023-05-17 01:26:55 +0300

    Добавил завершение процессов в start.sh

--- start.sh
+++ start.sh
@@ -21,3 +21,32 @@ zrdn_0_pid=$!; echo "Запуск ЗРДН 0 PID=$zrdn_0_pid"
 zrdn_1_pid=$!; echo "Запуск ЗРДН 1 PID=$zrdn_1_pid"
 ./zrdn.sh 2 &
 zrdn_2_pid=$!; echo "Запуск ЗРДН 2 PID=$zrdn_2_pid"
+
+
+echo "Завершение работы подсистемы РЛС 0"
+disown $rls_0_pid 2>/dev/null
+kill -9 $rls_0_pid 2>/dev/null
+
+echo "Завершение работы подсистемы РЛС 1"
+disown $rls_1_pid 2>/dev/null
+kill -9 $rls_1_pid 2>/dev/null
+
+echo "Завершение работы подсистемы РЛС 2"
+disown $rls_2_pid 2>/dev/null
+kill -9 $rls_2_pid 2>/dev/null
+
+echo "Завершение работы подсистемы ЗРДН 0"
+disown $zrdn_0_pid 2>/dev/null
+kill -9 $zrdn_0_pid 2>/dev/null
+
+echo "Завершение работы подсистемы ЗРДН 1"
+disown $zrdn_1_pid 2>/dev/null
+kill -9 $zrdn_1_pid 2>/dev/null
+
+echo "Завершение работы подсистемы ЗРДН 2"
+disown $zrdn_2_pid 2>/dev/null
+kill -9 $zrdn_2_pid 2>/dev/null
+
+echo "Завершение работы подсистемы СПРО"
+disown $spro_pid 2>/dev/null
+kill -9 $spro_pid 2>/dev/null;
